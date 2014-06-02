f11590d926a396e29abd4ef9c10420b52101a77c
Ibragim Gapuraev <Sermilion@pc-197-178.customer.newc.carlton.lan>
2014-06-02 14:46:38 +0100

    Initial Commit

---
+++ ReadLater/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReadLater
+//
+//  Created by Ibragim Gapuraev on 02/06/2014.
+//  Copyright (c) 2014 Sermilion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReadLater/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReadLater
+//
+//  Created by Ibragim Gapuraev on 02/06/2014.
+//  Copyright (c) 2014 Sermilion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
