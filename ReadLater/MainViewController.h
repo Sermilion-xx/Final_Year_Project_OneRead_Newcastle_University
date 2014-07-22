55ac5178e65ecabb66af7d3c08db01dfa0661331
Ibragim Gapuraev <Sermilion@pc-197-178.customer.newc.carlton.lan>
2014-07-22 22:51:36 +0100

    Selected tags are highlighted in menu

--- ReadLater/MainViewController.h
+++ ReadLater/MainViewController.h
@@ -10,11 +10,35 @@
 #import "User.h"
 #import "Article.h"
 #import "LoginViewController.h"
+#import "Database.h"
+#import "DWTagList.h"
 
+@interface MainViewController : UIViewController <DWTagListDelegate>
 
-@interface MainViewController : UIViewController
-//<LoginDelegate>
+@property (nonatomic, strong) NSMutableArray        *array;
+@property (nonatomic, strong) DWTagList             *tagList;
+@property (nonatomic, strong) DWTagList             *blogList;
+//@property (nonatomic, weak) IBOutlet UITextField    *addTagField;
 
+- (IBAction)tappedAdd:(id)sender;
+
+@property (nonatomic, strong) IBOutlet UIButton* E;
+@property (nonatomic, strong) IBOutlet UIButton* R;
+@property (nonatomic, strong) IBOutlet UIButton* A;
+
+@property (nonatomic, strong) IBOutlet UIButton* allTagsButton;
+@property (nonatomic, strong) IBOutlet UIButton* allBlogsButton;
+@property (nonatomic, strong) IBOutlet UIButton* sortByDateButton;
+@property (nonatomic, strong) IBOutlet UIButton* sortByRatingButton;
+@property (nonatomic, strong) IBOutlet UIButton* goButton;
+
+@property (nonatomic, strong) NSMutableArray* allTags;
+@property (nonatomic, strong) NSMutableArray* allBlogs;
+@property (nonatomic, strong) NSMutableArray* selectedTags;
+@property (nonatomic, strong) NSMutableArray* selectedBlogs;
+@property (nonatomic, strong) Database* db;
+//0 - E, 1- R, 2-A
+@property (nonatomic, assign) NSInteger ERA;
 
 
 @end
--- ReadLater/MainViewController.m
+++ ReadLater/MainViewController.m
@@ -8,12 +8,41 @@
 
 #import "MainViewController.h"
 #import "User.h"
+#import "DWTagList.h"
 
 @interface MainViewController ()
 
 @end
 
 @implementation MainViewController
+
+@synthesize allBlogs, allTags, db;
+
+- (Database* ) db
+{
+    if (!db) {
+        db = [[Database alloc] init];
+    }
+    return db;
+}
+
+- (NSMutableArray* ) allBlogs
+{
+    if (!allBlogs) {
+        allBlogs = [[NSMutableArray alloc] initWithCapacity:20];
+    }
+    
+    return allBlogs;
+}
+
+- (NSMutableArray* ) allTags
+{
+    if (!allTags) {
+        allTags = [[NSMutableArray alloc] initWithCapacity:20];
+    }
+    
+    return allTags;
+}
 
 
 
@@ -31,8 +60,71 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.db openDatabase];
+    self.allTags = [self.db getAllTags];
+    self.allBlogs = [self.db getAllBlog];
+    [self.db closeDatabase];
+    
+    _tagList = [[DWTagList alloc] initWithFrame:CGRectMake(30.0f, 180.0f, self.view.bounds.size.width-40.0f, 50.0f)];
+    [_tagList setAutomaticResize:YES];
+    _array = [[NSMutableArray alloc] initWithArray:self.allTags];
+    [_tagList setTags:_array];
+    [_tagList setTagDelegate:self];
+    
+    // Customisation
+    [_tagList setCornerRadius:4.0f];
+    [_tagList setBorderColor:[UIColor lightGrayColor].CGColor];
+    [_tagList setBorderWidth:1.0f];
+    
+    _blogList = [[DWTagList alloc] initWithFrame:CGRectMake(30.0f, 320.0f, self.view.bounds.size.width-40.0f, 50.0f)];
+    [_blogList setAutomaticResize:YES];
+    _array = [[NSMutableArray alloc] initWithArray:self.allBlogs];
+    [_blogList setTags:_array];
+    [_blogList setTagDelegate:self];
+    
+    // Customisation
+    [_blogList setCornerRadius:4.0f];
+    [_blogList setBorderColor:[UIColor lightGrayColor].CGColor];
+    [_blogList setBorderWidth:1.0f];
+    
+    [self.view addSubview:_blogList];
+    [self.view addSubview:_tagList];
+    
 }
+
+- (void)selectedTag:(NSString *)tagName tagIndex:(NSInteger)tagIndex
+{
+//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message"
+//                                                    message:[NSString stringWithFormat:@"You tapped tag %@ at index %ld", tagName,(long)tagIndex]
+//                                                   delegate:nil
+//                                          cancelButtonTitle:@"Ok"
+//                                          otherButtonTitles:nil];
+//    [alert show];
+
+    [self.selectedTags addObject:tagName];
+    
+    
+}
+
+- (IBAction)ETapped:(id)sender
+{
+    
+}
+- (IBAction)tappedAdd:(id)sender
+{
+//    [_addTagField resignFirstResponder];
+//    if ([[_addTagField text] length]) {
+//        [_array addObject:[_addTagField text]];
+//    }
+//    [_addTagField setText:@""];
+//    [_tagList setTags:_array];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -40,27 +132,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    // Return the number of sections.
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    // Return the number of rows in the section.
-    //NSLog(@"numberOfRowsInSection: self.articles: %lu", (unsigned long)self.articles.count);
-    return 1;
-}
-
--(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    //cell.backgroundColor = [self colorForIndex:indexPath.row];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"ContentCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    return cell;
-}
 @end
